58a619b2c6246afb62964cab6b0fa0b7fdac491f
hoanganhquan23 <hoanganhquan23@gmail.com>
2024-12-10 01:25:41 +0700

    post

---
+++ demo_tx2/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace demo_tx2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_ma = new System.Windows.Forms.TextBox();
+            this.txt_ten = new System.Windows.Forms.TextBox();
+            this.txt_dc = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnTim = new System.Windows.Forms.Button();
+            this.datanhanvien = new System.Windows.Forms.DataGridView();
+            this.manv = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hoten = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.diachi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.datanhanvien)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(412, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã Nhân Viên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(412, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Họ Tên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(412, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Địa Chỉ";
+            // 
+            // txt_ma
+            // 
+            this.txt_ma.Location = new System.Drawing.Point(569, 128);
+            this.txt_ma.Name = "txt_ma";
+            this.txt_ma.Size = new System.Drawing.Size(142, 20);
+            this.txt_ma.TabIndex = 3;
+            // 
+            // txt_ten
+            // 
+            this.txt_ten.Location = new System.Drawing.Point(569, 185);
+            this.txt_ten.Name = "txt_ten";
+            this.txt_ten.Size = new System.Drawing.Size(142, 20);
+            this.txt_ten.TabIndex = 4;
+            // 
+            // txt_dc
+            // 
+            this.txt_dc.Location = new System.Drawing.Point(569, 248);
+            this.txt_dc.Name = "txt_dc";
+            this.txt_dc.Size = new System.Drawing.Size(142, 20);
+            this.txt_dc.TabIndex = 5;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(904, 139);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(97, 40);
+            this.btnThem.TabIndex = 6;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(904, 223);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(97, 40);
+            this.btnSua.TabIndex = 7;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(904, 306);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(97, 40);
+            this.btnXoa.TabIndex = 8;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnTim
+            // 
+            this.btnTim.Location = new System.Drawing.Point(904, 389);
+            this.btnTim.Name = "btnTim";
+            this.btnTim.Size = new System.Drawing.Size(97, 40);
+            this.btnTim.TabIndex = 9;
+            this.btnTim.Text = "Tìm";
+            this.btnTim.UseVisualStyleBackColor = true;
+            this.btnTim.Click += new System.EventHandler(this.btnTim_Click);
+            // 
+            // datanhanvien
+            // 
+            this.datanhanvien.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datanhanvien.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.manv,
+            this.hoten,
+            this.diachi});
+            this.datanhanvien.Location = new System.Drawing.Point(350, 345);
+            this.datanhanvien.Name = "datanhanvien";
+            this.datanhanvien.RowHeadersWidth = 51;
+            this.datanhanvien.Size = new System.Drawing.Size(509, 234);
+            this.datanhanvien.TabIndex = 10;
+            this.datanhanvien.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.datanhanvien_CellContentClick);
+            // 
+            // manv
+            // 
+            this.manv.DataPropertyName = "manv";
+            this.manv.HeaderText = "Mã NV";
+            this.manv.MinimumWidth = 6;
+            this.manv.Name = "manv";
+            this.manv.Width = 125;
+            // 
+            // hoten
+            // 
+            this.hoten.DataPropertyName = "hoten";
+            this.hoten.HeaderText = "Họ Tên ";
+            this.hoten.MinimumWidth = 6;
+            this.hoten.Name = "hoten";
+            this.hoten.Width = 125;
+            // 
+            // diachi
+            // 
+            this.diachi.DataPropertyName = "diachi";
+            this.diachi.HeaderText = "Địa chỉ";
+            this.diachi.MinimumWidth = 6;
+            this.diachi.Name = "diachi";
+            this.diachi.Width = 125;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(904, 471);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(97, 40);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1209, 731);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.datanhanvien);
+            this.Controls.Add(this.btnTim);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txt_dc);
+            this.Controls.Add(this.txt_ten);
+            this.Controls.Add(this.txt_ma);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.datanhanvien)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_ma;
+        private System.Windows.Forms.TextBox txt_ten;
+        private System.Windows.Forms.TextBox txt_dc;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnTim;
+        private System.Windows.Forms.DataGridView datanhanvien;
+        private System.Windows.Forms.DataGridViewTextBoxColumn manv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hoten;
+        private System.Windows.Forms.DataGridViewTextBoxColumn diachi;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ demo_tx2/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace demo_tx2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // tạo đối tượng sử dụng tài liệu xml
+        XmlDocument doc = new XmlDocument();
+        // đường dẫn tới file xml
+        string tentep = @"C:\Users\DELL\source\repos\demo\demo\dsnhanvien.xml";
+        int d; // xác định chỉ số dòng được chọn trên dataGrid
+
+        private void HienThi()
+        {
+            datanhanvien.Rows.Clear(); // Xóa tất cả các dòng cũ trong DataGridView
+            doc.Load(tentep); // Tải file XML
+
+            // Lấy danh sách các nút <nhanvien>
+            XmlNodeList DS = doc.SelectNodes("/ds/nhanvien");
+            datanhanvien.ColumnCount = 3;
+
+            // Duyệt qua từng nút <nhanvien>
+            foreach (XmlNode nhan_vien in DS)
+            {
+                // Tạo một dòng mới trong DataGridView
+                int index = datanhanvien.Rows.Add();
+                DataGridViewRow row = datanhanvien.Rows[index];
+
+                // Truy xuất giá trị thuộc tính và nút con
+                XmlNode ma_nv = nhan_vien.SelectSingleNode("@manv");
+                XmlNode ho_ten = nhan_vien.SelectSingleNode("hoten");
+                XmlNode dia_chi = nhan_vien.SelectSingleNode("diachi");
+
+                // Gán giá trị vào các ô của dòng
+                row.Cells[0].Value = ma_nv?.InnerText ?? string.Empty;
+                row.Cells[1].Value = ho_ten?.InnerText ?? string.Empty;
+                row.Cells[2].Value = dia_chi?.InnerText ?? string.Empty;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HienThi();
+        }
+    
+       
+        private void datanhanvien_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Kiểm tra xem dòng được chọn có phải dòng dữ liệu hợp lệ
+            if (e.RowIndex >= 0) // Đảm bảo không phải dòng tiêu đề hoặc dòng trống
+            {
+                d = e.RowIndex;
+
+                // Lấy dữ liệu từ các ô trong dòng được chọn
+                txt_ma.Text = datanhanvien.Rows[d].Cells[0].Value?.ToString() ?? string.Empty;
+                txt_ten.Text = datanhanvien.Rows[d].Cells[1].Value?.ToString() ?? string.Empty;
+                txt_dc.Text = datanhanvien.Rows[d].Cells[2].Value?.ToString() ?? string.Empty;
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            // Đọc tài liệu XML
+            doc.Load(tentep);
+            XmlElement goc = doc.DocumentElement;
+
+            // Kiểm tra nếu mã nhân viên đã tồn tại
+            XmlNode existingNode = goc.SelectSingleNode($"/ds/nhanvien[@manv='{txt_ma.Text}']");
+            if (existingNode != null)
+            {
+                MessageBox.Show("Mã nhân viên đã tồn tại. Vui lòng nhập mã khác!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Tạo một nút <nhanvien>
+            XmlNode nhan_vien = doc.CreateElement("nhanvien");
+
+            // Tạo thuộc tính manv
+            XmlAttribute ma_nv = doc.CreateAttribute("manv");
+            ma_nv.InnerText = txt_ma.Text;
+            nhan_vien.Attributes.Append(ma_nv);
+
+            // Tạo nút <hoten>
+            XmlNode ho_ten = doc.CreateElement("hoten");
+            ho_ten.InnerText = txt_ten.Text;
+            nhan_vien.AppendChild(ho_ten);
+
+            // Tạo nút <diachi>
+            XmlNode dia_chi = doc.CreateElement("diachi");
+            dia_chi.InnerText = txt_dc.Text;
+            nhan_vien.AppendChild(dia_chi);
+
+            // Thêm nút nhanvien vào gốc
+            goc.AppendChild(nhan_vien);
+
+            // Lưu tệp XML
+            doc.Save(tentep);
+
+            // Hiển thị lại dữ liệu
+            HienThi();
+            MessageBox.Show("Thêm nhân viên thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+     
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            // Đọc tệp XML
+            doc.Load(tentep);
+            XmlElement goc = doc.DocumentElement;
+
+            // Xác định nút cần xóa
+            XmlNode nhan_vien_xoa = goc.SelectSingleNode("/ds/nhanvien[@manv='" + txt_ma.Text + "']");
+
+            // Xóa nút nhân viên
+            goc.RemoveChild(nhan_vien_xoa);
+
+            // Lưu tệp XML
+            doc.Save(tentep);
+
+            // Hiển thị lại dữ liệu
+            HienThi();
+
+            // Thông báo xóa thành công
+            MessageBox.Show("Xóa nhân viên thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            doc.Load(tentep);
+            XmlElement goc = doc.DocumentElement;
+            // xac dinh nut nhan vien can sua
+            XmlNode nhan_vien_cu = goc.SelectSingleNode("/ds/nhanvien[@manv = '" + txt_ma.Text + "']");
+
+            XmlNode nhan_vien_moi = doc.CreateElement("nhanvien");
+
+            XmlAttribute ma_nv = doc.CreateAttribute("manv");
+            ma_nv.InnerText = txt_ma.Text;
+            nhan_vien_moi.Attributes.Append(ma_nv);
+
+            XmlNode ho_ten = doc.CreateElement("hoten");
+            ho_ten.InnerText = txt_ten.Text;
+            nhan_vien_moi.AppendChild(ho_ten);
+
+            XmlNode dia_chi = doc.CreateElement("diachi");
+            dia_chi.InnerText = txt_dc.Text;
+            nhan_vien_moi.AppendChild(dia_chi);
+
+            // thay nut nhan vien cu bang nhan vien moi
+            goc.ReplaceChild(nhan_vien_moi, nhan_vien_cu);
+            // Lưu tệp XML
+            doc.Save(tentep);
+
+            // Hiển thị lại dữ liệu
+            HienThi();
+
+            // Thông báo sửa thành công
+            MessageBox.Show("Sửa thông tin nhân viên thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+        }
+
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Tải tệp XML
+                doc.Load(tentep);
+
+                // Lấy gốc của tệp XML
+                XmlElement goc = doc.DocumentElement;
+
+                // Xác định mã cần tìm
+                string manvCanTim = "nv01";
+
+                // Tìm nút nhân viên có mã "nv01"
+                XmlNode nhan_vien_tim = goc.SelectSingleNode($"/ds/nhanvien[@manv='{manvCanTim}']");
+
+                // Kiểm tra nếu không tìm thấy
+                if (nhan_vien_tim == null)
+                {
+                    MessageBox.Show("Không tìm thấy nhân viên với mã: " + manvCanTim, "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                // Xóa dữ liệu cũ trên DataGridView
+                datanhanvien.Rows.Clear();
+
+                // Lấy thông tin nhân viên
+                string manv = nhan_vien_tim.Attributes["manv"].InnerText;
+                string hoten = nhan_vien_tim.SelectSingleNode("hoten").InnerText;
+                string diachi = nhan_vien_tim.SelectSingleNode("diachi").InnerText;
+
+                // Thêm thông tin nhân viên tìm được vào DataGridView
+                datanhanvien.Rows.Add();
+                datanhanvien.Rows[0].Cells[0].Value = manv;
+                datanhanvien.Rows[0].Cells[1].Value = hoten;
+                datanhanvien.Rows[0].Cells[2].Value = diachi;
+
+                // Thông báo tìm kiếm thành công
+                MessageBox.Show("Tìm thấy nhân viên có mã: " + manvCanTim, "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                // Thông báo lỗi nếu có
+                MessageBox.Show("Lỗi khi tìm nhân viên: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Gọi lại hàm HienThi() để hiển thị toàn bộ danh sách ban đầu
+                HienThi();
+
+                // Xóa các thông tin hiển thị trên các TextBox (nếu có)
+                txt_ma.Clear();
+                txt_ten.Clear();
+                txt_dc.Clear();
+
+                // Thông báo reset thành công (tùy chọn)
+                MessageBox.Show("Danh sách đã được đặt lại.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                // Thông báo lỗi nếu có
+                MessageBox.Show("Lỗi khi đặt lại danh sách: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
